f91adef90a1d6c5c946924653743cb2b86f0cf21
Stefan <stefan@razorsecure.com>
2024-07-03 23:56:18 +0100

    Code cleanup.

--- image_sampler.py
+++ image_sampler.py
@@ -1,13 +1,16 @@
-from PIL import Image # pip install pillow
-import tkinter as tk # sudo apt-get install python3-tk
+from PIL import Image
+import tkinter as tk
 from tkinter import filedialog
 import sample_calculator
 
 SAMPLE_SIZE = 3
 
+
 def parse_input(type):
     while True:
-        number = input("Please specify the " + type + " in pixels of each sample: ")
+        number = input("Please specify the " +
+                       type +
+                       " in pixels of each sample: ")
         try:
             val = int(number)
             if val < 0:
@@ -15,15 +18,16 @@ def parse_input(type):
                 continue
             break
         except ValueError:
-            print("Error: input must be a positive integer.")    
+            print("Error: input must be a positive integer.")
     return val
 
-def crop_image(image: object, 
+
+def crop_image(image: object,
                sample_width: int,
                sample_height: int,
-               x: int, 
+               x: int,
                y: int):
-    
+
     # Generates a cropped image.
 
     left = x
@@ -36,7 +40,8 @@ def crop_image(image: object,
 if __name__ == "__main__":
     sample_width = parse_input("width")
     sample_height = parse_input("height")
-    input("A menu will now appear, from which an image should be selected.\nPress Enter when ready.")
+    input("A menu will now appear, "
+          "from which an image should be selected.\nPress Enter when ready.")
     root = tk.Tk()
     root.withdraw()
 
@@ -45,13 +50,17 @@ if __name__ == "__main__":
     image = Image.open(filename)
     width, height = image.size
 
-    # At this point, the sample calculator calculates the set of coordinates for generating each sample.
-    image_positions = sample_calculator.run(width, height, sample_width, sample_height, SAMPLE_SIZE)
+    # At this point, the sample calculator calculates
+    # the set of coordinates for generating each sample.
+    image_positions = sample_calculator.run(
+        width, height, sample_width, sample_height, SAMPLE_SIZE)
     print(image_positions)
     images = []
     for i in image_positions:
-        cropped_image = crop_image(image, sample_width, sample_height, i[0], i[1])
+        cropped_image = crop_image(
+            image, sample_width, sample_height, i[0], i[1])
         images.append(cropped_image)
-        
-        # Each cropped image is displayed using the system's default image viewer.
+
+        # Each cropped image is displayed
+        # using the system's default image viewer.
         cropped_image.show()
--- sample_calculator.py
+++ sample_calculator.py
@@ -1,38 +1,53 @@
 from random import randint, choice
 import math
 
-"""
-This module determines the positions to use for each image sample, based on the input provided in the run() method.
+'''
+This module determines the positions to use for each image sample,
+based on the input provided in the run() method.
 
-Initially, the code divides the main image's field into an n*n grid of equally sized subdivisions, where n is an integer between 1 and 3.
+Initially, the code divides the main image field into an n*n grid
+of equally sized subdivisions, where n is an integer between 1 and 3.
 
-Next, each of the three samples to be generated is uniquely assigned to one of these subdivisions selected at random, preventing any overlapping samples.
+Next, each of the three samples to be generated is uniquely assigned
+to one of these subdivisions selected at random,
+preventing any overlapping samples.
 
-Finally, the position of each sample within its respective subdivision is calculated. This involves applying a random offset to each sample.
+Finally, the position of each sample within its respective
+subdivision is calculated. This involves applying
+a random offset to each sample.
 
-The resulting positions of each sample are then returned to the invoking module, which generates the samples using the supplied positions.
+The resulting positions of each sample are then returned
+to the invoking module, which generates the samples
+using the supplied positions.
+'''
 
-"""
-
-FIELD = [0,0]
+FIELD = [0, 0]
 SAMPLE_WIDTH = 0
 SAMPLE_HEIGHT = 0
 SAMPLE_SIZE = 3
 
+
 def error_check():
-    
-    # This ensures that the image is large enough to accommodate all the required samples.
+
+    # This ensures that the image is large enough
+    # to accommodate all the required samples.
 
     if SAMPLE_WIDTH * SAMPLE_SIZE > FIELD[0]:
         if SAMPLE_HEIGHT * SAMPLE_SIZE > FIELD[1]:
-            raise ValueError(f"Cannot take {SAMPLE_SIZE} non-overlapping samples of dimension {SAMPLE_WIDTH}x{SAMPLE_HEIGHT} in field of size {FIELD[0]}x{FIELD[1]}.")
+            raise ValueError(f"Cannot take {SAMPLE_SIZE} non-overlapping"
+                             f"samples of dimension"
+                             f"{SAMPLE_WIDTH}x{SAMPLE_HEIGHT}"
+                             f"in field of size {FIELD[0]}x{FIELD[1]}.")
+
 
 def calculate_subdivision_array_dimension(sample_length, field_length):
-    
-    # This determines whether the image will be divided into one, two, or three subdivisions along a given axis.
+
+    # This determines whether the image will be divided
+    # into one, two, or three subdivisions along a given axis.
 
     if sample_length > field_length:
-        raise ValueError(f"Sample dimension ({sample_length}) greater than image dimension ({field_length}).")
+        raise ValueError(f"Sample dimension {sample_length} is"
+                         f"greater than image dimension {field_length}.")
     quotient = field_length / sample_length
     if quotient >= 3:
         return 3
@@ -40,20 +55,22 @@ def calculate_subdivision_array_dimension(sample_length, field_length):
         return 2
     return 1
 
-def calculate_sample_position(
-    
-    # Within each occupied subdivision, the position of the sample to be generated is calculated at random.
 
-        subdivision_index_x, 
+def calculate_sample_position(
+        subdivision_index_x,
         subdivision_index_y,
         subdivision_width,
-        subdivision_height   
-    ):
+        subdivision_height
+        ):
+
+    # Within each occupied subdivision, the position
+    # of the sample to be generated is calculated at random.
     x_displacement = randint(0, subdivision_width - SAMPLE_WIDTH)
     y_displacement = randint(0, subdivision_height - SAMPLE_HEIGHT)
     x = ((subdivision_index_x) * subdivision_width) + x_displacement
     y = ((subdivision_index_y) * subdivision_height) + y_displacement
-    return [x,y]
+    return [x, y]
+
 
 def run(field_x, field_y, sample_width, sample_height, sample_size):
     global FIELD, SAMPLE_WIDTH, SAMPLE_HEIGHT, SAMPLE_SIZE
@@ -62,20 +79,30 @@ def run(field_x, field_y, sample_width, sample_height, sample_size):
     SAMPLE_HEIGHT = sample_height
     SAMPLE_SIZE = sample_size
     error_check()
-    subdivision_array_width = calculate_subdivision_array_dimension(SAMPLE_WIDTH,FIELD[0])
-    subdivision_array_height =  calculate_subdivision_array_dimension(SAMPLE_HEIGHT,FIELD[1])
+    subdivision_array_width = calculate_subdivision_array_dimension(
+                            SAMPLE_WIDTH, FIELD[0])
+    subdivision_array_height = calculate_subdivision_array_dimension(
+                            SAMPLE_HEIGHT, FIELD[1])
     subdivision_width = math.floor(FIELD[0]/subdivision_array_width)
     subdivision_height = math.floor(FIELD[1]/subdivision_array_height)
-    # Generating the array of subdivisions into which the image will be divided.
-    subdivision_array_x_values = [i for i in range(0,subdivision_array_width)]
-    subdivision_array_y_values = [j for j in range(0,subdivision_array_height)]
-    subdivision_array = [[k, l] for k in subdivision_array_x_values for l in subdivision_array_y_values]
+    # Generating the array of subdivisions
+    # into which the image will be divided.
+    subdivision_array_x_values = [
+        i for i in range(0, subdivision_array_width)]
+    subdivision_array_y_values = [
+        j for j in range(0, subdivision_array_height)]
+    subdivision_array = [
+        [k, l] for k in subdivision_array_x_values
+        for l in subdivision_array_y_values]
     samples = []
     for n in range(0, SAMPLE_SIZE):
-        # Each sample is assigned to a subdivision at random. Its position within its subdivision is then calculated.
+        # Each sample is assigned to a subdivision at random.
+        # Its position within its subdivision is then calculated.
         subdivision_indices = choice(subdivision_array)
         subdivision_array.remove(subdivision_indices)
-        samples.append(calculate_sample_position(subdivision_indices[0],subdivision_indices[1],subdivision_width,subdivision_height))
-    return samples  
-
-
+        samples.append(calculate_sample_position(
+            subdivision_indices[0],
+            subdivision_indices[1],
+            subdivision_width,
+            subdivision_height))
+    return samples
